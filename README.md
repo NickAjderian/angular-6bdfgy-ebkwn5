ad81f4c2173c5c96c925c4f9fb7e01a4de6d255e
NickAjderian <nick@flikk.net>
2020-06-11 14:34:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-6bdfgy-ebkwn5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-6bdfgy-ebkwn5)
---
+++ src/app/menu-position-example.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Menu positioning
+ */
+@Component({
+  selector: 'menu-position-example',
+  templateUrl: 'menu-position-example.html',
+  styleUrls: ['menu-position-example.css'],
+})
+export class MenuPositionExample {}
+
+
+/**  Copyright 2019 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
